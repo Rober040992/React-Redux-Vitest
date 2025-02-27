dcedfdd7a6fa55997ee4c6aea9b2eee561bc0438
RoberGF <rgfrasta@gmail.com>
2025-02-27 19:16:37 +0100

    actions types and creators

---
+++ src/store/actions.ts
@@ -0,0 +1,40 @@
+import type { Advert } from "@/pages/adverts/types";
+
+// action Types
+type AuthLogin = {
+  type: "auth/login";
+};
+
+type AuthLogout = {
+  type: "auth/logout";
+};
+
+type AdvertsLoaded = {
+  type: "advert/loaded";
+  payload: Advert[];
+};
+
+type AdvertCreated = {
+  type: "advert/created";
+  payload: Advert;
+};
+
+//Action creators
+export const authLogin = (): AuthLogin => ({
+  type: "auth/login",
+});
+export const authLout = (): AuthLogout => ({
+  type: "auth/logout",
+});
+
+export const advertsLoaded = (adverts: Advert[]): AdvertsLoaded => ({
+  type: "advert/loaded",
+  payload: adverts,
+});
+
+export const advertsCreated = (advert: Advert): AdvertCreated => ({
+  type: "advert/created",
+  payload: advert,
+});
+
+export type Actions = AuthLogin | AuthLogout | AdvertsLoaded | AdvertCreated;
