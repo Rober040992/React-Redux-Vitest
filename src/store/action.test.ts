963a5ed691bb49e2151a4655da090df47f7cb92e
RoberGF <rgfrasta@gmail.com>
2025-03-09 19:03:00 +0100

    test on advertsLoadedFulfilled

--- src/store/action.test.ts
+++ src/store/action.test.ts
@@ -1,12 +1,30 @@
-import {describe, expect, test } from "vitest";
-
-import { authLoginPending } from "./actions";
+import { describe, expect, test } from "vitest";
+import { advertsLoadedFulfilled, authLoginPending } from "./actions";
 describe("authLoginPending", () => {
-    test("should return auth/login/pending action", () => {
-        const action = {
-            type: "auth/login/pending",
-        }
-        const result = authLoginPending()
-        expect(result).toEqual(action)
-    })
-})
+  test("should return auth/login/pending action", () => {
+    const action = {
+      type: "auth/login/pending",
+    };
+    const result = authLoginPending();
+    expect(result).toEqual(action);
+  });
+});
+
+describe("AdvertsLoadedFulfilled", () => {
+  test("should return adverts/loaded/fulfilled action", () => {
+    const advertExample = [{
+        id: "1",
+        name: "string",
+        sale: false,
+        price: 1,
+        tags: ["work"],
+        photo: null,
+        }]
+    const action = {
+      type: "advert/loaded/fulfilled",
+      payload: advertExample,
+    };
+    const result = advertsLoadedFulfilled(advertExample);
+    expect(result).toEqual(action);
+  });
+});
